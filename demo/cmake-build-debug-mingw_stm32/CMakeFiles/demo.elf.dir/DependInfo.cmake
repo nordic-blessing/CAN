539347896563fe7bda0fa242442be2b711f49289
Icol Lee <Icol_Boom@outlook.com>
2024-09-15 16:53:40 +0800

    add demo

---
+++ demo/cmake-build-debug-mingw_stm32/CMakeFiles/demo.elf.dir/DependInfo.cmake
@@ -0,0 +1,65 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "E:/myscu/LEGION/learn/CAN/demo/Startup/startup_stm32f030f4px.s" "E:/myscu/LEGION/learn/CAN/demo/cmake-build-debug-mingw_stm32/CMakeFiles/demo.elf.dir/Startup/startup_stm32f030f4px.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F030x6"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "E:/myscu/LEGION/learn/CAN/demo/Inc"
+  "E:/myscu/LEGION/learn/CAN/demo/Drivers/STM32F0xx_HAL_Driver/Inc"
+  "E:/myscu/LEGION/learn/CAN/demo/Drivers/STM32F0xx_HAL_Driver/Inc/Legacy"
+  "E:/myscu/LEGION/learn/CAN/demo/Drivers/CMSIS/Device/ST/STM32F0xx/Include"
+  "E:/myscu/LEGION/learn/CAN/demo/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "E:/myscu/LEGION/learn/CAN/demo/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal.c" "CMakeFiles/demo.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal.c.obj" "gcc" "CMakeFiles/demo.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal.c.obj.d"
+  "E:/myscu/LEGION/learn/CAN/demo/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_cortex.c" "CMakeFiles/demo.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_cortex.c.obj" "gcc" "CMakeFiles/demo.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_cortex.c.obj.d"
+  "E:/myscu/LEGION/learn/CAN/demo/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_dma.c" "CMakeFiles/demo.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_dma.c.obj" "gcc" "CMakeFiles/demo.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_dma.c.obj.d"
+  "E:/myscu/LEGION/learn/CAN/demo/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_exti.c" "CMakeFiles/demo.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_exti.c.obj" "gcc" "CMakeFiles/demo.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_exti.c.obj.d"
+  "E:/myscu/LEGION/learn/CAN/demo/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash.c" "CMakeFiles/demo.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash.c.obj" "gcc" "CMakeFiles/demo.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash.c.obj.d"
+  "E:/myscu/LEGION/learn/CAN/demo/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash_ex.c" "CMakeFiles/demo.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/demo.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash_ex.c.obj.d"
+  "E:/myscu/LEGION/learn/CAN/demo/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_gpio.c" "CMakeFiles/demo.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_gpio.c.obj" "gcc" "CMakeFiles/demo.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_gpio.c.obj.d"
+  "E:/myscu/LEGION/learn/CAN/demo/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c.c" "CMakeFiles/demo.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c.c.obj" "gcc" "CMakeFiles/demo.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c.c.obj.d"
+  "E:/myscu/LEGION/learn/CAN/demo/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c_ex.c" "CMakeFiles/demo.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c_ex.c.obj" "gcc" "CMakeFiles/demo.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c_ex.c.obj.d"
+  "E:/myscu/LEGION/learn/CAN/demo/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr.c" "CMakeFiles/demo.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr.c.obj" "gcc" "CMakeFiles/demo.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr.c.obj.d"
+  "E:/myscu/LEGION/learn/CAN/demo/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr_ex.c" "CMakeFiles/demo.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/demo.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr_ex.c.obj.d"
+  "E:/myscu/LEGION/learn/CAN/demo/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc.c" "CMakeFiles/demo.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc.c.obj" "gcc" "CMakeFiles/demo.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc.c.obj.d"
+  "E:/myscu/LEGION/learn/CAN/demo/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc_ex.c" "CMakeFiles/demo.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/demo.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc_ex.c.obj.d"
+  "E:/myscu/LEGION/learn/CAN/demo/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim.c" "CMakeFiles/demo.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim.c.obj" "gcc" "CMakeFiles/demo.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim.c.obj.d"
+  "E:/myscu/LEGION/learn/CAN/demo/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim_ex.c" "CMakeFiles/demo.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/demo.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim_ex.c.obj.d"
+  "E:/myscu/LEGION/learn/CAN/demo/Src/main.c" "CMakeFiles/demo.elf.dir/Src/main.c.obj" "gcc" "CMakeFiles/demo.elf.dir/Src/main.c.obj.d"
+  "E:/myscu/LEGION/learn/CAN/demo/Src/stm32f0xx_hal_msp.c" "CMakeFiles/demo.elf.dir/Src/stm32f0xx_hal_msp.c.obj" "gcc" "CMakeFiles/demo.elf.dir/Src/stm32f0xx_hal_msp.c.obj.d"
+  "E:/myscu/LEGION/learn/CAN/demo/Src/stm32f0xx_it.c" "CMakeFiles/demo.elf.dir/Src/stm32f0xx_it.c.obj" "gcc" "CMakeFiles/demo.elf.dir/Src/stm32f0xx_it.c.obj.d"
+  "E:/myscu/LEGION/learn/CAN/demo/Src/syscalls.c" "CMakeFiles/demo.elf.dir/Src/syscalls.c.obj" "gcc" "CMakeFiles/demo.elf.dir/Src/syscalls.c.obj.d"
+  "E:/myscu/LEGION/learn/CAN/demo/Src/sysmem.c" "CMakeFiles/demo.elf.dir/Src/sysmem.c.obj" "gcc" "CMakeFiles/demo.elf.dir/Src/sysmem.c.obj.d"
+  "E:/myscu/LEGION/learn/CAN/demo/Src/system_stm32f0xx.c" "CMakeFiles/demo.elf.dir/Src/system_stm32f0xx.c.obj" "gcc" "CMakeFiles/demo.elf.dir/Src/system_stm32f0xx.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
